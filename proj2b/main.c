765dfaa8494d8739790c7264febe35dc94721abf
blezzoh <iradub0@sewanee.edu>
2022-12-02 14:17:41 -0800

    proj2b

--- proj2b/main.c
+++ proj2b/main.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int main(int argc, char *argv[]){
-    if(argc == 3){
+int main(int argc, char *argv[])
+{
+    if (argc == 3)
+    {
         // input, output file
         FILE *file_in;
         FILE *file_out;
 
-        // string 
+        // string
         char *input = argv[1];
         char *output = argv[2];
 
         // input file, output file
-        file_in= fopen(input, "r");
+        file_in = fopen(input, "r");
         file_out = fopen(output, "w");
-        if( file_in == NULL || file_out == NULL){
+        if (file_in == NULL || file_out == NULL)
+        {
             printf("Error cannot open one of the files");
             exit(1);
         }
 
-        int **content = malloc(5*5 * sizeof(int));
+        int content[5][5];
         int CONTENT_SIZE = 5;
-       
-        
-        for(int i =0; i<5; i++){
+
+        for (int i = 0; i < 5; i++)
+        {
+            // read 5 int
             fread(content[i], sizeof(int), CONTENT_SIZE, file_in);
-            fseek(file_in, 40, SEEK_CUR);
-            printf("%d\n", i);
+            // skip 5 int
+            fseek(file_in, 5 * sizeof(int), SEEK_CUR);
+            
+            // write 5 int on 5 lines
+            for(int j = 0; j<5; j++){
+                fprintf(file_out, "%d\n",content[i][j]);
+            }
         }
 
-        printf("\n n %d ", content[0][2]);
         fclose(file_in);
         fclose(file_out);
-        free(content);
         exit(0);
-
     }
-    else{
+    else
+    {
         printf("problem, you have %d args, you need only 3 arguments\n", argc);
     }
 }
